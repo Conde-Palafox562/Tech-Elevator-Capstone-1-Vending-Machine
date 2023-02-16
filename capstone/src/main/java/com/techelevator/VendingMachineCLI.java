35432dd7779f5b463dd3c287b0578a50bbbd51f3
Steven Brooks <stevenbrooks77@gmail.com>
2023-02-16 09:43:28 -0500

    Capstone1Steven

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -7,6 +7,7 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_EXIT = "Exit";
 
 	private Menu menu;
 
@@ -22,6 +23,8 @@ public class VendingMachineCLI {
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+			} else if(choice.equals(MAIN_MENU_EXIT)) {
+				break;
 			}
 		}
 	}
