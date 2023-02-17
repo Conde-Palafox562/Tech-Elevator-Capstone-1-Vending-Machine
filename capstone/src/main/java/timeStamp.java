580604bf7ed123a13ef765d8c8e0ccda46072a6d
Steven Brooks <stevenbrooks77@gmail.com>
2023-02-17 18:12:52 -0500

    Cap1Steven

---
+++ capstone/src/main/java/timeStamp.java
@@ -0,0 +1,27 @@
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.io.PrintWriter;
+import java.time.format.DateTimeFormatter;
+
+public class timeStamp {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+
+    //PrintWriter writer = new PrintWriter();
+            //
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
