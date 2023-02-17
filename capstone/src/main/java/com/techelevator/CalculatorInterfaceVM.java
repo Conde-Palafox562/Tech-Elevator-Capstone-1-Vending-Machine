e38004a79e3ecc2d69ef3572dcae0d3d7ddf7635
Raul Conde <r.conde2017@gmail.com>
2023-02-17 13:58:18 -0500

    Capstone Updates Raul v10

--- capstone/src/main/java/com/techelevator/CalculatorInterfaceVM.java
+++ capstone/src/main/java/com/techelevator/CalculatorInterfaceVM.java
@@ -1,11 +1,59 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 public class CalculatorInterfaceVM {
 
+    private PrintWriter out;
+    private Scanner in;
 
+    public CalculatorInterfaceVM(InputStream input, OutputStream output) {
+        this.out = new PrintWriter(output);
+        this.in = new Scanner(input);
+    }
 
+    public static void finishTransaction() {
+        //return change
+        System.out.println("Change dispensed: $" + vendingMachine.getBalance());
+
+        BigDecimal changeBalance = vendingMachine.getBalance();
+
+        BigDecimal quarterValue = new BigDecimal("0.25");
+        int quarters = 0;
+
+        BigDecimal dimeValue = new BigDecimal("0.10");
+        int dimes = 0;
+
+        BigDecimal nickelValue = new BigDecimal("0.05");
+        int nickels = 0;
+
+        while (vendingMachine.getBalance().compareTo(quarterValue) >= 0) {
+            quarters++;
+            vendingMachine.getBalance() = vendingMachine.getBalance().subtract(quarterValue);
+        }
+
+        while (vendingMachine.getBalance().compareTo(dimeValue) >= 0) {
+            dimes++;
+            vendingMachine.getBalance() = vendingMachine.getBalance().subtract(dimeValue);
+        }
+
+        while (vendingMachine.getBalance().compareTo(nickelValue) >= 0) {
+            nickels++;
+            vendingMachine.getBalance() = vendingMachine.getBalance().subtract(nickelValue);
+        }
+
+        System.out.println("Quarters: " + quarters + ", " + "Dimes: " + dimes + ", " + "Nickels: " + nickels);
+
+        //zero out balance
+        vendingMachine.getBalance() = vendingMachine.getBalance().subtract(balance);
+
+        out.print(System.lineSeparator() + "Please take your chance. Thank you and come again!");
+        out.flush();
+
+    }
 }
