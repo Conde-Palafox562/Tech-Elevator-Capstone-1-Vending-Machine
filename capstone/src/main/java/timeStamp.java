f744ee75eb2a1dd827049122b92fe913ad7dd104
Steven Brooks <stevenbrooks77@gmail.com>
2023-02-20 09:19:21 -0500

    Cap1Steven02202023

--- capstone/src/main/java/timeStamp.java
+++ capstone/src/main/java/timeStamp.java
@@ -5,11 +5,18 @@ import java.io.PrintWriter;
 import java.time.format.DateTimeFormatter;
 
 public class timeStamp {
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
-    LocalDateTime now = LocalDateTime.now();
+    //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+   // LocalDateTime now = LocalDateTime.now();
+    //System.out.println(dtf.format(now));
+
+    //Need Vending Log Class.. make the formatted time
+    //setLoggedActivity
+    //logFeedMoney
+    //logSelectedProduct
+
 
     //PrintWriter writer = new PrintWriter();
-            //
+            //try open PrintWriter for loop need
 
 
 }
